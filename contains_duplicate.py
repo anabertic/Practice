16314e1b2e47929b52d4e9bd6a034ff384ff6066
Ana Bertić <ana.bertic2@gmail.com>
2025-11-18 08:50:48 +0000

    Create contains_duplicate.py

---
+++ contains_duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq_set = set()
+        for num in nums:
+            if num in freq_set:
+                return True
+            freq_set.add(num) 
+        return False
+        
