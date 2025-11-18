f1b86da0fc87b1033dcb2dfbc7a3d291b56ed5db
Ana Bertić <ana.bertic2@gmail.com>
2025-11-18 08:49:33 +0000

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
+        for i in range(0, len(nums)):
+            if target - nums[i] in lookup: # da li sam vidla ovaj broj prije?
+                return (i, lookup[target-nums[i]])
+            lookup[nums[i]] = i
+        
