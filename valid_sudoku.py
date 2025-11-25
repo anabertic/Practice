3e9ba189cc515e59e1daf5dc0c331d9c1e1346d8
Ana Bertić <ana.bertic2@gmail.com>
2025-11-25 08:50:17 +0000

    Update valid_sudoku.py

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -2,5 +2,20 @@
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(0,9):
-            for j in range(0,0)
+            seen_row = [set() for _ in range(0,9)]
+            seen_column = [set() for _ in range(0,9)]
+            seen_boxes = [set() for _ in range(0,9)]
+            for row in range(0,9):
+                for column in range(0,9):
+                    if board[row][column] != '.':
+                        if board[row][column] in seen_row[row]:
+                            return False
+                        seen_row[row].add(board[row][column])
+                        if board[row][column] in seen_column[column]:
+                            return False
+                        seen_column[column].add(board[row][column]) 
+                        if board[row][column] in seen_boxes[(row//3)*3+(column//3)]:
+                            return False
+                        seen_boxes[(row//3)*3+(column//3)].add(board[row][column])
+            return True
+            
