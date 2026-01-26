5e87cb1d6e0381c6cdd528a9332ec4b59916bfa3
Ana Bertić <ana.bertic2@gmail.com>
2026-01-26 09:45:17 +0000

    Update ___Remove Node From End of Linked List.py

--- ___Remove Node From End of Linked List.py
+++ ___Remove Node From End of Linked List.py
@@ -3,6 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        first, second = dummy, dummy
+        for i in range(0, n+1):
+            first = first.next
+        while first:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return dummy.next
+
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
